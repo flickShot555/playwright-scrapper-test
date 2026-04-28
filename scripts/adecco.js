e29e21c789d6d6cca71a55af9206481b58cdbb89
wofacode <138582634+flickShot555@users.noreply.github.com>
2026-04-28 14:57:37 +0500

    level1-implementation-done

--- scripts/adecco.js
+++ scripts/adecco.js
@@ -4,12 +4,27 @@ const delay = (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
 };
 
+async function handleCookiesButton(page){
+    try{
+        await page.waitForSelector("#onetrust-accept-btn-handler", {timeout:5000});
+        await page.click("#onetrust-accept-btn-handler");
+        console.log("#cookies policy button clicked");
+        
+        await delay(2000);
+
+    }catch{
+        console.log("No Cookies button appeared, continuing anyways!");
+    }
+}
+
+
 async function main(){
     const browser = await chromium.launch({headless:false});
     const context = await browser.newContext();
     const page = await context.newPage();
 
-    await page.goto("https://jobs.de.adecco.com/");
+    await page.goto("https://jobs.de.adecco.com/", {waitUntil: "networkidle"});
+    await handleCookiesButton(page);
 
     await delay(3000);
 
